bc86b528477934d7cbc0d7b6ae3e1efe0106ced9
Paulo Arce <codebypaulo@gmail.com>
2026-01-28 19:39:51 -0300

    hola mundo en javascript y caracteristicas basicas

---
+++ 01-fundamentos-javascript/00-hello-world.js
@@ -0,0 +1,18 @@
+// Esto es un comentario de una sola linea
+/* Esto es
+un comentario
+en varias lineas
+*/
+
+console.log("Hola mundo!!");
+console.log("Hola javascript");
+console.log(`Hola javascript`);
+
+console.log("5");
+console.log(5);
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(5 ** 2);
