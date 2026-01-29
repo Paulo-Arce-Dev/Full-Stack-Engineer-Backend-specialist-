82e61a28c68ec4e88d6a5bbaac586119af2a6556
Paulo Arce <codebypaulo@gmail.com>
2026-01-29 17:28:54 -0300

    resolviendo - mensaje de bienvenida

---
+++ 01-fundamentos-javascript/01-variables/ejercicios/05-mensaje-bienvenida.js
@@ -0,0 +1,8 @@
+// Ejercicio - Mensaje de Bienvenida
+
+const nombreUsuario = "Elon";
+let actividad = "meditación";
+let tiempoActividadEnMinutos = 15;
+
+let frase = `Hola ${nombreUsuario}, hoy completaste tu rutina de ${actividad} en ${tiempoActividadEnMinutos} minutos. ¡Buen trabajo!`;
+console.log(frase);
