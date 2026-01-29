629a49885c0116d643b091da92e31867ee9f365f
Paulo Arce <codebypaulo@gmail.com>
2026-01-29 17:27:06 -0300

    resolviendo ejercicio - validador de datos

---
+++ 01-fundamentos-javascript/01-variables/ejercicios/02-validador-datos.js
@@ -0,0 +1,13 @@
+// Ejercicio - El validador de datos
+
+const nombre = "Lionel Messi";
+let edad = 38;
+let mayorDeEdad = true;
+const fechaNacimiento = "24/06/1987";
+let ciudad = "Rosario";
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof mayorDeEdad);
+console.log(typeof fechaNacimiento);
+console.log(typeof ciudad);
