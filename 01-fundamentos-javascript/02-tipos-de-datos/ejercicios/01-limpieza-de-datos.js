8fb92b26bc155bc28788886c7c09db60460284db
Paulo Arce <codebypaulo@gmail.com>
2026-01-31 19:31:53 -0300

    resolviendo ejercicio

---
+++ 01-fundamentos-javascript/02-tipos-de-datos/ejercicios/01-limpieza-de-datos.js
@@ -0,0 +1,5 @@
+// Ejercicio - Limpieza de Datos (Strings)
+
+let entradaUsuario = " mArCos pEreZ ";
+let resultado = entradaUsuario.trim().toUpperCase();
+console.log(resultado);
---
+++ 01-fundamentos-javascript/02-tipos-de-datos/ejercicios/02-buscador-palabras.js
@@ -0,0 +1,7 @@
+// Ejercicio - Buscador de Palabras (Booleans & Strings)
+
+let descripcion =
+  "Este televisor 4K cuenta con tecnología OLED y sonido Dolby Atmos.";
+
+let tiene4K = descripcion.includes("4K");
+console.log(tiene4K);
